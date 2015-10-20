39738dc3e0244c3aecdafd6c0c4269bb7c70fda5
gcch <black.che.r.ry.karat.5@gmail.com>
2015-10-20 10:08:30 +0900

    Update 81chinachu-sleep.sh

--- 81chinachu-sleep.sh
+++ 81chinachu-sleep.sh
@@ -19,7 +19,6 @@ MARGIN_BOOT=600
 
 case ${1} in
 	hibernate|suspend)
-		echo "This system is waking up now."
 		NEXT_PROG_START_TIME=`chinachu-get-next-time ${CHINACHU_URL}`
 		WAKEUP_TIME=`expr ${NEXT_PROG_START_TIME} - ${MARGIN_BOOT}`
 		if `date -d @${WAKEUP_TIME} +%s > ${WAKEALARM}`; then
@@ -27,9 +26,10 @@ case ${1} in
 		else
 			echo "[`date +"${DATE_FORMAT}"`] ${0}: failure to schedule" 1>&2
 		fi
+		echo "This system will be stop soon."
 	;;
 	thaw|resume)
 		echo 0 > ${WAKEALARM}
-		echo "This system will be stop soon."
+		echo "This system is waking up now."
 	;;
 esac
