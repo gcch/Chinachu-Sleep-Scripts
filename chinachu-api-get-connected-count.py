b3c489f1604b3757d7b28fee1add8fcda7be10b2
gcch <black.che.r.ry.karat.5@gmail.com>
2015-10-22 10:07:36 +0900

    Update chinachu-api-get-connected-count.py

--- chinachu-api-get-connected-count.py
+++ chinachu-api-get-connected-count.py
@@ -17,7 +17,7 @@ import time
 from urllib.request import urlopen
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Get the number of users who is connecting Chinachu')
+	parser = argparse.ArgumentParser(description='Get the number of users connecting to Chinachu')
 	parser.add_argument('url', type=str, help='url string (example: http://localhost:10772)')
 	args = parser.parse_args()
 
