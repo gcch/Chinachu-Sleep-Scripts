ff02497b70de299e8e4950be2c0d5c87fc497e76
gcch <black.che.r.ry.karat.5@gmail.com>
2015-10-22 13:15:15 +0900

    Update chinachu-check-status.sh

--- chinachu-check-status.sh
+++ chinachu-check-status.sh
@@ -21,8 +21,8 @@ MARGIN_SLEEP=1800
 
 # check the status of Chinachu: connected count
 if [ `chinachu-api-get-connected-count ${CHINACHU_URL}` -gt 0 ]; then
-        echo "[`date +"${DATE_FORMAT}"`] ${0}: Someone is connecting to Chinachu WUI." 1>&2
-        exit 1
+	echo "[`date +"${DATE_FORMAT}"`] ${0}: Someone is connecting to Chinachu WUI." 1>&2
+	exit 1
 fi
 
 # ------------------------------------------------------- #
@@ -52,19 +52,19 @@ NOW=`date +%s`
 UPTIME=$(ALARM_DATE=$(cat /proc/driver/rtc | egrep 'alrm_time|alrm_date' | echo $(sed -e "s/^alrm_time.* \([0-9]\+:[0-9]\+:[0-9]\+\)$/\1/" -e "s/^alrm_date.* \([0-9]\+-[0-9]\+-[0-9]\+\)$/\1/") UTC) ; date -d "${ALARM_DATE}" +%s)
 BORDER=$((${UPTIME} + ${MARGIN_UPTIME}))
 if [ ${NOW} -lt ${BORDER} ]; then
-        echo "[`date +"${DATE_FORMAT}"`] ${0}: It has not elapsed only a few minutes from a boot. (uptime: `date -d @${UPTIME} +"${DATE_FORMAT}"`)" 1>&2
-        exit 1
+	echo "[`date +"${DATE_FORMAT}"`] ${0}: It has not elapsed only a few minutes from a boot. (uptime: `date -d @${UPTIME} +"${DATE_FORMAT}"`)" 1>&2
+	exit 1
 fi
 
 # ------------------------------------------------------- #
 
 # check whether someone is accessing this server via Samba (root only can execute normally)
 if type smbstatus 1>/dev/null 2>&1; then
-        SMB_USERS=`smbstatus -p | grep "^[0-9]" | wc -l`
-        if [ ${SMB_USERS} -gt 0 ]; then
-                echo "[`date +"${DATE_FORMAT}"`] ${0}: Someone is accessing this server via Samba." 1>&2
-        fi
-        exit 1
+	SMB_USERS=`smbstatus -p | grep "^[0-9]" | wc -l`
+	if [ ${SMB_USERS} -gt 0 ]; then
+		echo "[`date +"${DATE_FORMAT}"`] ${0}: Someone is accessing this server via Samba." 1>&2
+		exit 1
+	fi
 fi
 
 # ------------------------------------------------------- #
